99fc972aedd046de4b970c2c7aa518fe56ebf6cb
Nikhil Eashy P <70377480+nikhileashy@users.noreply.github.com>
2024-03-06 03:35:37 -0800

    Delete Frontend directory

--- Frontend/src/pages/logindata.js
+++
@@ -1,9 +0,0 @@
-// import { useState } from "react";
-
-// const [isloggedin, setisloggedin] = useState(false);
-let isloggedin = false
-function changestatus(status) {
-    isloggedin = status;
-}
-
-export { isloggedin, changestatus };
